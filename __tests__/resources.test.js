e625a09fd6d320a8a2006b4cf18d5262b99a72a1
Elise Muellerleile <eliamue@live.com>
2021-08-23 18:19:01 -0500

    insert resource passing test, models and controllers built for city, state, and category, resource tweaked

--- __tests__/resources.test.js
+++ __tests__/resources.test.js
@@ -2,22 +2,29 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import City from '../lib/Model/City.js';
+import State from '../lib/Model/State.js';
+import Category from '../lib/Model/Category.js';
 // import Resource from '../lib/models/Resource.js';
 
 describe('demo CRUD routes', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     return setup(pool);
   });
 
   it('tests create resource route', async () => {
+    await State.insert({ us_state: 'MN' });
+    await City.insert({ city: 'Minneapolis', state_id: 1 });
+    await Category.insert({ category: 'Butt' });
+
     const resource = {
       src_name: 'Tubman Family Crisis and Support Services',
       src_description: 'We help people with stuff',
       st_address: '4432 Chicago Avenue South',
-      city_id: 2,
+      city_id: 1,
       county: 'Hennepin',
-      zip: 55407,
-      state_id: 2,
+      zip: '55407',
+      state_id: 1,
       main_number: '612-825-0000',
       alt_number: '612-825-3333',
       email: '',
@@ -31,7 +38,7 @@ describe('demo CRUD routes', () => {
 
     expect(res.body).toEqual({
       ...resource,
-      id: 1
+      id: '1'
     });
   });
 });
