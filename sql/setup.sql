8e0cce025c3b2ccc9893200df35874575ffea204
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-08-23 11:51:23 -0700

    Reformatted file structure, worked on setup sql, create resource test

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,7 +1,38 @@
-DROP TABLE IF EXISTS villagers;
+DROP TABLE IF EXISTS resources CASCADE;
+DROP TABLE IF EXISTS us_states CASCADE;
+DROP TABLE IF EXISTS cities CASCADE;
+DROP TABLE IF EXISTS categories;
 
-CREATE TABLE villagers (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    vid INT NOT NULL,
-    name TEXT NOT NULL
+ CREATE TABLE resources (
+      id SERIAL PRIMARY KEY NOT NULL,
+      name TEXT NOT NULL,
+      src_description TEXT NOT NULL,
+      st_address TEXT,
+      city_id INTEGER NOT NULL,
+      zip TEXT,
+      county TEXT,
+      state_id INTEGER NOT NULL,
+      main_number VARCHAR(20) NOT NULL,
+      alt_number VARCHAR(20),
+      email VARCHAR(256),
+      website TEXT,
+      category_id INTEGER NOT NULL,
+      tags TEXT[],
+);
+
+CREATE TABLE us_states (
+      id SERIAL PRIMARY KEY NOT NULL REFERENCES resources(state_id),
+      us_state TEXT NOT NULL
+      
+);
+
+CREATE TABLE cities (
+      id SERIAL PRIMARY KEY NOT NULL REFERENCES resources(city_id),
+      city TEXT NOT NULL,
+      state_id INTEGER NOT NULL REFERENCES us_states(id)
+);
+
+CREATE TABLE categories (
+      id SERIAL PRIMARY KEY NOT NULL REFERENCES resources(category_id),
+      category TEXT NOT NULL
 );
