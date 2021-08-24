a5803533e098d51f70881b4850e025a62a5f6119
Brianna Bias <brianna.bias@gmail.com>
2021-08-24 13:12:27 -0700

    get by id test passing for real resources

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,37 +1,23 @@
 DROP TABLE IF EXISTS resources CASCADE;
-DROP TABLE IF EXISTS us_states CASCADE;
 DROP TABLE IF EXISTS cities CASCADE;
 DROP TABLE IF EXISTS categories CASCADE;
 
-CREATE TABLE us_states (
-      id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-      us_state TEXT NOT NULL
-);
 
 CREATE TABLE cities (
       id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-      city TEXT NOT NULL,
-      state_id INTEGER NOT NULL REFERENCES us_states(id)
-);
+      city TEXT NOT NULL);
 
 CREATE TABLE categories (
       id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
       category TEXT NOT NULL
 );
 
- CREATE TABLE resources (
+CREATE TABLE resources (
       id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
       src_name TEXT NOT NULL,
       src_description TEXT NOT NULL,
-      st_address TEXT,
       city_id INTEGER NOT NULL REFERENCES cities(id),
-      zip TEXT,
-      county TEXT,
-      state_id INTEGER NOT NULL REFERENCES us_states(id),
-      main_number VARCHAR(20) NOT NULL,
-      alt_number VARCHAR(20),
-      email VARCHAR(256),
-      website TEXT,
+      info TEXT [],
       category_id INTEGER NOT NULL REFERENCES categories(id),
       tags TEXT[]
 );
