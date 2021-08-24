62eb4c92fbee53bac51f79359eb4fdbaac0b0161
Elise Muellerleile <79237986+eliamue@users.noreply.github.com>
2021-08-24 11:10:52 -0500

    Data modeling (#15)
    * init pls
    
    * inity
    
    * init 2
    
    * butter
    
    * insert resource passing test, models and controllers built for city, state, and category, resource tweaked
    
    * gitignore nonsense
    
    * comment
    
    * dummy data added to appease the github gods
    
    * FIX IT FFS
    
    * require put in string in pooljs
    
    * require added to nodejs yml
    
    * yml file debug
    
    * Delete .env

--- __tests__/resources.test.js
+++ __tests__/resources.test.js
@@ -2,30 +2,36 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Resources from '../lib/models/Resources.js';
+import City from '../lib/Model/City.js';
+import State from '../lib/Model/State.js';
+import Category from '../lib/Model/Category.js';
+// import Resource from '../lib/models/Resource.js';
+// I hate everything EVEN MORE
 
 describe('demo CRUD routes', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     return setup(pool);
   });
 
   it('tests create resource route', async () => {
+    await State.insert({ us_state: 'MN' });
+    await City.insert({ city: 'Minneapolis', state_id: 1 });
+    await Category.insert({ category: 'Butt' });
+
     const resource = {
-      name: 'Tubman Family Crisis and Support Services',
-      address: '4432 Chicago Avenue South',
-      city: 'Minneapolis',
-      city_id: 2,
+      src_name: 'Tubman Family Crisis and Support Services',
+      src_description: 'We help people with stuff',
+      st_address: '4432 Chicago Avenue South',
+      city_id: 1,
       county: 'Hennepin',
-      zip: 55407,
-      usstate: 'MN',
-      state_id: 2,
-      number: '612-825-0000',
-      number2: '612-825-3333',
+      zip: '55407',
+      state_id: 1,
+      main_number: '612-825-0000',
+      alt_number: '612-825-3333',
       email: '',
       website: 'https://www.tubman.org/',
-      category: 'Mental Health',
       category_id: 1,
-      subcategory: 'General',
+      tags: ['General', 'butts', 'butter'],
     };
     const res = await request(app)
       .post('/api/v1/resources')
@@ -33,7 +39,7 @@ describe('demo CRUD routes', () => {
 
     expect(res.body).toEqual({
       ...resource,
-      id: 1
+      id: '1'
     });
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,38 +1,37 @@
 DROP TABLE IF EXISTS resources CASCADE;
 DROP TABLE IF EXISTS us_states CASCADE;
 DROP TABLE IF EXISTS cities CASCADE;
-DROP TABLE IF EXISTS categories;
-
- CREATE TABLE resources (
-      id SERIAL PRIMARY KEY NOT NULL,
-      name TEXT NOT NULL,
-      src_description TEXT NOT NULL,
-      st_address TEXT,
-      city_id INTEGER NOT NULL,
-      zip TEXT,
-      county TEXT,
-      state_id INTEGER NOT NULL,
-      main_number VARCHAR(20) NOT NULL,
-      alt_number VARCHAR(20),
-      email VARCHAR(256),
-      website TEXT,
-      category_id INTEGER NOT NULL,
-      tags TEXT[],
-);
+DROP TABLE IF EXISTS categories CASCADE;
 
 CREATE TABLE us_states (
-      id SERIAL PRIMARY KEY NOT NULL REFERENCES resources(state_id),
+      id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
       us_state TEXT NOT NULL
-      
 );
 
 CREATE TABLE cities (
-      id SERIAL PRIMARY KEY NOT NULL REFERENCES resources(city_id),
+      id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
       city TEXT NOT NULL,
       state_id INTEGER NOT NULL REFERENCES us_states(id)
 );
 
 CREATE TABLE categories (
-      id SERIAL PRIMARY KEY NOT NULL REFERENCES resources(category_id),
+      id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
       category TEXT NOT NULL
+);
+
+ CREATE TABLE resources (
+      id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      src_name TEXT NOT NULL,
+      src_description TEXT NOT NULL,
+      st_address TEXT,
+      city_id INTEGER NOT NULL REFERENCES cities(id),
+      zip TEXT,
+      county TEXT,
+      state_id INTEGER NOT NULL REFERENCES us_states(id),
+      main_number VARCHAR(20) NOT NULL,
+      alt_number VARCHAR(20),
+      email VARCHAR(256),
+      website TEXT,
+      category_id INTEGER NOT NULL REFERENCES categories(id),
+      tags TEXT[]
 );
