9bcc0d12085c19deca6a163b388b59f1acb17e89
Brianna Bias <brianna.bias@gmail.com>
2021-08-25 12:00:31 -0700

    get all cities test passing

--- __tests__/cities.test.js
+++ __tests__/cities.test.js
@@ -4,10 +4,6 @@ import request from 'supertest';
 import app from '../lib/app.js';
 import City from '../lib/Model/City.js';
 
-const city = { city: 'Portland' };
-const city2 = { city: 'Minneapolis' };
-const city3 = { city: 'San Jose' };
-const city4 = { city: 'Reno' };
 
 describe('demo routes', () => {
   beforeEach(() => {
@@ -25,13 +21,21 @@ describe('demo routes', () => {
     });
   });
   it('gets all cities via GET', async () => {
-    await City.insert(city, city2, city3, city4);
+    const city1 = { city: 'Portland' };
+    const city2 = { city: 'Minneapolis' };
+    const city3 = { city: 'San Jose' };
+    const city4 = { city: 'Reno' };
+
+    await City.insert(city1);
+    await City.insert(city2);
+    await City.insert(city3);
+    await City.insert(city4);
 
     const res = await request(app).get('/api/v1/cities');
     expect(res.body).toEqual([
       {
         id: '1',
-        ...city
+        ...city1
       },
       {
         id: '2',
