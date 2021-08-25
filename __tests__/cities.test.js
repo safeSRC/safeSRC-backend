703bdaf83698ab45e9944f818649d66569b81c58
Brianna Bias <brianna.bias@gmail.com>
2021-08-25 11:35:37 -0700

    create cities test written and passing

--- __tests__/cities.test.js
+++ __tests__/cities.test.js
@@ -1,16 +1,21 @@
 import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
-// import request from 'supertest';
-// import app from '../lib/app.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+import City from '../lib/Model/City.js';
 
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it.skip('passes CI', () => {
-    const passTest = 'test passed';
+  it('creates a city via POST', async () => {
+    // const city = await City.insert({ city: 'Portland' });
+    const res = await request(app).post('/api/v1/cities').send({ city: 'Portland' });
 
-    expect(passTest).toEqual(passTest);
+    expect(res.body).toEqual({
+      id: '1',
+      city: 'Portland'
+    });
   });
 });
