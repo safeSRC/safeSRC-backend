e2f346ef8cb372879d5d277f75bee7fdf3d045b9
BenFrancka <benfrancka@gmail.com>
2021-08-26 09:51:36 -0700

    adds test for user signup

---
+++ __tests__/auth.tests.js
@@ -0,0 +1,20 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app/js';
+
+const agent = request.agent(app);
+
+describe('auth routes', () => {
+  beforeAll(() => {
+    return setup(pool);
+  });
+
+  it('signs a user up with POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({ email: 'email@email.com', password: 'password' });
+
+    expect(res.body).toEqual({ id: '1', email: 'email@email.com' });
+  });
+});
